cc8af75f37729e8b505bf575876421f1ae3b5bc8
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-22 16:42:23 -0300

    Initial items page construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { CharactersList } from '@/components/characterList'
+import { PageIntroBox } from '@/components/pageIntro'
+
 const HomePage = async () => {
   //
 
   return (
     <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
-      <div className="container md:px-6">Home Initial</div>
+      <div className="container md:px-6">
+        <PageIntroBox
+          title="Star Wars Characters"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        />
+        <CharactersList />
+      </div>
     </main>
   )
 }
