5ba6e5bd1ceed7e1e315e011c92ed3f3bc31499d
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-22 21:19:13 -0300

    Insert props on pageIntro component

--- src/components/pageIntro/interface.ts
+++ src/components/pageIntro/interface.ts
@@ -1,4 +1,5 @@
 export interface IPageIntro {
   title?: string
   subtitle?: string
+  className?: string
 }
--- src/components/pageIntro/pageIntro.component.tsx
+++ src/components/pageIntro/pageIntro.component.tsx
@@ -1,11 +1,11 @@
 import { IPageIntro } from './interface'
 
-export const PageIntroBox = ({ title, subtitle }: IPageIntro) => {
+export const PageIntroBox = ({ className, title, subtitle, ...props }: IPageIntro) => {
   const hasTitle = title && title.length
   const hasSubtitle = subtitle && subtitle.length
 
   return (
-    <section className="w-full mb-[3.125rem]">
+    <section className="w-full mb-[3.125rem]" {...props}>
       {hasTitle && <h1 className="text-3xl leading-[4rem] text-grayStarWars-300 mb-[1.563rem]">{title}</h1>}
       {hasSubtitle && <h2 className=" text-1xl m-0 max-w-[52%] lg:max-w-full">{subtitle}</h2>}
     </section>
