eb7cdb749fa33af39459e63d7ee96fd5d718c228
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-22 21:19:49 -0300

    Title list component - CharacterList

---
+++ src/components/characterList/titleList.component.tsx
@@ -0,0 +1,13 @@
+import { cn } from '@/utils/classes'
+import { IListTitle } from './interface'
+
+export const TitleCharactersList = ({ children, className, ...props }: IListTitle) => {
+  return (
+    <h2
+      className={cn('text-grayStarWars-300 text-2xl leading-10 font-light mt-[3.125rem] mb-10', className)}
+      {...props}
+    >
+      {children}
+    </h2>
+  )
+}
