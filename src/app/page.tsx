5bedc1b8e9641e450b40798dbdbb2f91c78eb7e5
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-24 19:33:52 -0300

    Back to the same code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,21 @@ import { Suspense } from 'react'
 import Loading from './loading'
 
 const HomePage = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 4000))
   const dataPeople = await listPeopleWithHomeworlds()
 
   return (
-    <Suspense fallback={<Loading />}>
-      <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
-        <div className="container lg:px-6">
-          <PageIntroBox
-            title="Star Wars Characters"
-            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-          />
-        </div>
+    <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
+      <div className="container lg:px-6">
+        <PageIntroBox
+          title="Star Wars Characters"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        />
+      </div>
 
-        <Suspense>
-          <CharactersList listTitle="All Characters" listItems={dataPeople} />
-        </Suspense>
-      </main>
-    </Suspense>
+      <Suspense fallback={<Loading />}>
+        <CharactersList listTitle="All Characters" listItems={dataPeople} />
+      </Suspense>
+    </main>
   )
 }
 
