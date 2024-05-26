7205be4ca5255c499284d78a4f6116c12f8a0788
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-25 23:08:15 -0300

    Suspense with loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,21 @@ import { listPeopleWithHomeworlds } from '@/api/services/people'
 import { CharactersList } from '@/components/characterList'
 import { PageIntroBox } from '@/components/pageIntro'
 import { Suspense } from 'react'
-import Loading from './_loading'
 
-const MyPageComponent = async () => {
+const HomePage = async () => {
   const dataPeople = await listPeopleWithHomeworlds()
 
   return (
-    <Suspense fallback={<Loading />}>
-      <CharactersList listTitle="All Characters" listItems={dataPeople} />
-    </Suspense>
-  )
-}
-
-const HomePage = async () => {
-  return (
-    <Suspense fallback={<Loading />}>
+    <Suspense>
       <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
         <div className="container lg:px-6">
           <PageIntroBox
             title="Star Wars Characters"
             subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
           />
-          <MyPageComponent />
         </div>
+
+        <CharactersList listTitle="All Characters" listItems={dataPeople} />
       </main>
     </Suspense>
   )
