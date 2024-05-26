23acf822fb17457156a067233d94af832e2c1e90
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-25 22:43:41 -0300

    Loading test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,31 @@ import { listPeopleWithHomeworlds } from '@/api/services/people'
 import { CharactersList } from '@/components/characterList'
 import { PageIntroBox } from '@/components/pageIntro'
 import { Suspense } from 'react'
-import Loading from './loading'
+import Loading from './_loading'
 
-const HomePage = async () => {
+const MyPageComponent = async () => {
   const dataPeople = await listPeopleWithHomeworlds()
 
   return (
-    <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
-      <div className="container lg:px-6">
-        <PageIntroBox
-          title="Star Wars Characters"
-          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-        />
-      </div>
+    <Suspense fallback={<Loading />}>
+      <CharactersList listTitle="All Characters" listItems={dataPeople} />
+    </Suspense>
+  )
+}
 
-      <Suspense fallback={<Loading />}>
-        <CharactersList listTitle="All Characters" listItems={dataPeople} />
-      </Suspense>
-    </main>
+const HomePage = async () => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <main className="min-h-screen bg-white text-grayStarWars-200 py-20 font-light">
+        <div className="container lg:px-6">
+          <PageIntroBox
+            title="Star Wars Characters"
+            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+          />
+          <MyPageComponent />
+        </div>
+      </main>
+    </Suspense>
   )
 }
 
